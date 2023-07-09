1ebe9a49cce41b4d36cc932bb6ff5cb8a6d82a26
nodegraph <nodegraph@gmail.com>
2023-07-08 20:10:23 -0700

    update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,7 +1,24 @@
 export const metadata = {
   title: 'YouMacro Video Downloader',
   description: 'Drag and drop any video from your browser. Download at the highest resolutions. No usage limits. No ads. 100% Free.',
-  image: "https://youmacro.com/images/screenshot.jpg"
+  image: "https://youmacro.com/images/screenshot.jpg",
+
+  openGraph: {
+    title: 'Next.js',
+    description: 'Drag and drop any video from your browser. Download at the highest resolutions. No usage limits. No ads. 100% Free.',
+    url: 'https://youmacro.com',
+    siteName: 'YouMacro Video Downloader',
+    images: [
+      {
+        url: 'https://youmacro.com/images/screenshot.jpg',
+        width: 2276,
+        height: 1280,
+        alt: 'YouMacro Video Downloader Screenshot',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 import Hero from '@/components/hero'
